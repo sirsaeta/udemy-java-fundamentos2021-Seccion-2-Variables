30f06db5a089a0f68e1ef4d1beb308ba184663ae
Axel Enrique Martinez Duran <U631378@TECO.COM.AR>
2021-05-04 15:23:05 -0300

    02-TiposEnterosYString

--- src/com/udemy/java/fundamentos/variables/Main.java
+++ src/com/udemy/java/fundamentos/variables/Main.java
@@ -3,6 +3,21 @@ package com.udemy.java.fundamentos.variables;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Definimos la variable
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+
+        double miVariableFlotante = 10.2;
+        System.out.println(miVariableFlotante);
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        System.out.println(miVariableEntera+miVariableFlotante);
+
+        String miVariableString = "Saludos Saeta";
+        System.out.println(miVariableString);
+        System.out.println(miVariableString+miVariableEntera);
+        miVariableString = "Adios Saeta";
+        System.out.println(miVariableString);
     }
 }
