d0145b1d4f7ba29251a34c5c89a4485d2e55edfc
Axel Enrique Martinez Duran <U631378@TECO.COM.AR>
2021-05-05 18:34:20 -0300

    05-ConcatenacionVariables

--- src/com/udemy/java/fundamentos/variables/Main.java
+++ src/com/udemy/java/fundamentos/variables/Main.java
@@ -34,5 +34,20 @@ public class Main {
         //var áVariable = 1;// no se recomienda utilizar pero se puede
         //var #miVariable = 1; no se puede utilizar caracteres especiales
         // var 1miVariable = 1; no se permite iniciar con numero
+
+        //Concatenar caracteres
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+
+        //Cuidado con las palabras reservadas
+        var i = 3;
+        var j = 4;
+        System.out.println(i + j);//se realiza la suma de numeros
+        System.out.println(i + j + usuario);//primero realiza la suma por estar a la izquierda y luego concatena
+        System.out.println(usuario + i + j);//si la primera es una cadena el resto
+        System.out.println(usuario + (i + j));//los parentesis modifica la prioridad primero sumando y luego concatena
+
     }
 }
